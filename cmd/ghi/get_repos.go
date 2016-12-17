d561f92ad4753054fe4955d6e2e79a700c37a0ea
Sunny <me@darkowlzz.space>
2016-12-17 20:57:16 +0530

    Fix get_repos with empty repo language

--- cmd/ghi/get_repos.go
+++ cmd/ghi/get_repos.go
@@ -16,6 +16,28 @@ type GetRepoOptions struct {
 	IsPrivate string
 }
 
+type Repo struct {
+	URL      string
+	Language string
+	Stars    int
+	Forks    int
+}
+
+func (r Repo) RepoString() []string {
+	return []string{r.URL, r.Language,
+		strconv.Itoa(r.Stars), strconv.Itoa(r.Forks)}
+}
+
+var newRepo = func(r *github.Repository) Repo {
+	var lang string
+	if r.Language != nil {
+		lang = *r.Language
+	} else {
+		lang = "-"
+	}
+	return Repo{*r.HTMLURL, lang, *r.StargazersCount, *r.ForksCount}
+}
+
 func NewCmdGetRepos(out io.Writer) *cobra.Command {
 	options := &GetRepoOptions{}
 
@@ -34,32 +56,39 @@ func NewCmdGetRepos(out io.Writer) *cobra.Command {
 	return cmd
 }
 
-func RunGetRepos(cmd *cobra.Command, args []string, out io.Writer, c *GetRepoOptions) error {
-	var user string
-	if len(args) > 0 {
-		return cmd.Help()
-	}
+var getRepos = func() ([]*github.Repository, error) {
 	client := rootCommand.gclient.GetClient()
-	if len(args) == 0 {
-		user = rootCommand.gclient.User
-	}
+	// User should be fetched only after the above client init, else user remains
+	// empty.
+	user := rootCommand.gclient.User
 	opt := &github.RepositoryListOptions{Type: "all", Sort: "updated"}
 	repos, _, err := client.Repositories.List(user, opt)
 	if err != nil {
-		exitWithError(fmt.Errorf("Error Getting Repos"))
+		return nil, err
 	}
+
 	if len(repos) == 0 {
 		fmt.Fprintf(os.Stderr, "No repos found\n")
-		return nil
+	}
+
+	return repos, nil
+}
+
+func RunGetRepos(cmd *cobra.Command, args []string, out io.Writer, c *GetRepoOptions) error {
+	if len(args) > 0 {
+		return cmd.Help()
+	}
+	repos, err := getRepos()
+	if err != nil {
+		return err
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"REPO", "LANGUAGE", "STARS", "FORKS"})
 
 	for _, repo := range repos {
-		table.Append([]string{*repo.HTMLURL, *repo.Language,
-			strconv.Itoa(*repo.StargazersCount),
-			strconv.Itoa(*repo.ForksCount)})
+		r := newRepo(repo)
+		table.Append(r.RepoString())
 	}
 
 	table.Render()
